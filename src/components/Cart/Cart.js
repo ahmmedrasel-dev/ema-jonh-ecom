b3df4294b1e1593b7347d4a6320eaede87acaf98
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-03-22 22:30:24 +0300

    calculation total price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Cart.css'
 const Cart = ({ cart }) => {
+
+  // Calculation Total Price, Shipping Cost, Tax
+  let total = 0;
+  let shippingCost = 0;
+
+  for (const product of cart) {
+    total = total + product.price;
+    shippingCost = shippingCost + product.shipping;
+  }
+
+  const tax = (total * .1).toFixed(2)
   return (
-    <div>
+    <div className='cart-ifo'>
       <h1>Order Summary</h1>
       <p>Selected Items: {cart.length}</p>
-      <p>Total Price: $1230.00</p>
-      <p>Total Shipping Charge: $10.00</p>
-      <p>Tax: $120</p>
+      <p>Total Price: ${total}</p>
+      <p>Shipping Charge: ${shippingCost}</p>
+      <p>Tax: ${tax}</p>
       <h3>Grand Total: $1559.00</h3>
       <button className='clear-cart'>Clear Cart</button>
       <button className='review-order'>Review Order</button>
